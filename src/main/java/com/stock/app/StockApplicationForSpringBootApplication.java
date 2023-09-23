b8b49e7e2b854352806aeadd091ece183b5a2d71
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-09-23 19:29:53 +0300

    Project created.

---
+++ src/main/java/com/stock/app/StockApplicationForSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.stock.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockApplicationForSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockApplicationForSpringBootApplication.class, args);
+	}
+
+}
