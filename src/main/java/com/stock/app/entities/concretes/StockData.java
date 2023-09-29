318d276a5c7b9fe7019b473a4a9a4078da5b50f4
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-09-29 14:18:28 +0300

    StockData entity update.

--- src/main/java/com/stock/app/entities/concretes/StockData.java
+++ src/main/java/com/stock/app/entities/concretes/StockData.java
@@ -2,58 +2,20 @@ package com.stock.app.entities.concretes;
 
 import lombok.Data;
 
-import java.util.List;
-import java.util.Map;
-
 @Data
 public class StockData {
-    private String _id;
-    private String ticker;
-    private String securityDesc;
-    private String country;
-    private String currency;
-    private int precision;
-    private String marketSector;
-    private String exchange;
-    private String legacyCode;
-    private String pttRow;
-    private int lotSize;
-    private boolean shortSellEnabled;
-    private int minimumLot;
-    private int maximumLot;
-    private String status;
-    private long listingDate;
-    private String grossSettl;
-    private String unit;
-    private String security;
-    private String type;
-    private String securityType;
-    private String code;
-    private String domain;
-    private String issuer;
-    private String issuerName;
-    private String subMarket;
-    private String subMarketDesc;
-    private String market;
-    private String marketDesc;
-    private String ISIN;
-    private String sector;
-    private List<String> index;
-    private Map<String, IndexWeight> indexWeight;
-    private long lastUpdateTime;
-
-
-    public static class IndexWeight {
-        private String tnos;
-        private long tnosd;
-        private String ffr;
-        private double ffrd;
-        private String ffs;
-        private long ffsd;
-        private String wf;
-        private int wfd;
-        private String w;
-        private double wd;
-
-    }
+    private String dusuk;
+    private String hacimlot;
+    private String acilis;
+    private String simge;
+    private String satis;
+    private String aciklama;
+    private String son;
+    private String hacimtl;
+    private String gunlukyuzde;
+    private String yuksek;
+    private String alis;
+    private String fark;
+    private String sembol;
+    private String dunkukapanis;
 }
