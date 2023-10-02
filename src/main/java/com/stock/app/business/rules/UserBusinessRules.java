80a187f04b6e7f92d401112e6ade7248559db2d6
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-10-02 13:53:52 +0300

    UserBusinessRules created.

---
+++ src/main/java/com/stock/app/business/rules/UserBusinessRules.java
@@ -0,0 +1,24 @@
+package com.stock.app.business.rules;
+
+import com.stock.app.core.utilies.exceptions.BusinnessException;
+import com.stock.app.dataAccess.abstracts.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class UserBusinessRules {
+    private UserRepository userRepository;
+
+    public void checkIfUserEmailExists(String email) {
+        if (this.userRepository.existsByEmail(email)) {
+            throw new BusinnessException("User email already exists");
+        }
+    }
+
+    public void checkIfUserNameExists(String name) {
+        if (this.userRepository.existsByEmail(name)) {
+            throw new BusinnessException("Username already exists");
+        }
+    }
+}
