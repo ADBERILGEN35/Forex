a13f9f1f06f3d429438201d2cfcfc8db0dd87647
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-10-05 19:04:03 +0300

    Update.

--- src/main/java/com/stock/app/StockApplicationForSpringBootApplication.java
+++ src/main/java/com/stock/app/StockApplicationForSpringBootApplication.java
@@ -1,13 +1,21 @@
 package com.stock.app;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StockApplicationForSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockApplicationForSpringBootApplication.class, args);
+	}
+
+
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
 	}
 
 }
