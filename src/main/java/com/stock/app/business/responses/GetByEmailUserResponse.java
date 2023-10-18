47016199b07d6b76f6c20cb4dbdce5606dc33cb5
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-10-18 18:07:38 +0300

    Forex APP || Renamed.

--- src/main/java/com/stock/app/business/responses/GetByEmailUserResponse.java
+++ src/main/java/com/stock/app/business/responses/GetByEmailUserResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class GetByEmailUserResonse {
+public class GetByEmailUserResponse {
     private String userName;
     private String password;
     private String TCKN;
--- src/main/java/com/stock/app/business/responses/GetByUserNameUserResponse.java
+++ src/main/java/com/stock/app/business/responses/GetByUserNameUserResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class GetByIdUserResonse {
+public class GetByUserNameUserResponse {
     private String userName;
     private String password;
     private String TCKN;
