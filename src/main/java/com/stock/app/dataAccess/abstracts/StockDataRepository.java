82e9a0e08329ec97181f526ed4f83b8672943e4c
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-10-19 19:12:19 +0300

    Forex APP || StockDataRepository updated.

--- src/main/java/com/stock/app/dataAccess/abstracts/StockDataRepository.java
+++ src/main/java/com/stock/app/dataAccess/abstracts/StockDataRepository.java
@@ -1,7 +1,22 @@
 package com.stock.app.dataAccess.abstracts;
 
 import com.stock.app.entities.concretes.StockData;
+import com.stock.app.entities.concretes.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface StockDataRepository extends JpaRepository<StockData,String> {
+
+    @Query("SELECT r FROM StockData r WHERE r.code LIKE %:code%")
+    List<StockData> findByCode(String code);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE StockData r SET r.lastprice = :lastprice WHERE r.code = :code")
+    void updateStockDataLastPrice(String code, Double lastprice);
+
 }
